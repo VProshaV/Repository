a2ab883752a625ebab6d9003d1f72965f639dd29
VProshaV <proscha_sob@mail.ru>
2021-11-29 01:31:48 +0300

    Add all my files

--- app/src/main/java/com/vova/example/myfirstapplication/MainActivity.kt
+++ app/src/main/java/com/vova/example/myfirstapplication/MainActivity.kt
@@ -1,11 +1,25 @@
 package com.vova.example.myfirstapplication
 
+import android.content.Intent
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.Button
+
+private const val HELLO_KEY = "Hello"
 
 class MainActivity : AppCompatActivity() {
+    lateinit var nextActivityButton: Button
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        nextActivityButton = findViewById(R.id.next_activity_button)
+
+        nextActivityButton.setOnClickListener {
+           val secondActivityIntent:Intent = Intent(this, SecondActivity::class.java)
+           secondActivityIntent.putExtra(HELLO_KEY,"Hello from MainActivity")
+
+            startActivity(secondActivityIntent)
+        }
     }
 }
